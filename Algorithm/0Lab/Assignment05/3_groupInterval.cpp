fbc8a7832abf3a06cfccc2c9578d80317240eee4
aanshik-dev <aanshiksinghtomar@gmail.com>
2025-09-10 14:15:58 +0530

    Assignment

---
+++ Algorithm/0Lab/Assignment05/3_groupInterval.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<pair<int, int>>> groupInterval(vector<pair<int, int>> &gaps) {
+  sort(gaps.begin(), gaps.end());
+  vector<vector<pair<int, int>>> res;
+}
+
+int main() {
+  vector<pair<int, int>> intervals = {{1, 3}, {2, 4}, {3, 5}, {7, 8}};
+  return 0;
+}
