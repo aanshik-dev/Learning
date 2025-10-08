2ee51da04ecbdff548c6f9a4b87d742259ee353b
aanshik-dev <aanshiksinghtomar@gmail.com>
2025-10-08 14:22:53 +0530

    Assignment

---
+++ Algorithm/0Lab/Assignment06/frogPath copy.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+pair<int, int> path(vector<int> &nums) {
+  vector<int> dp(nums.size(), 0);
+  vector<int> path(nums.size(), 0);
+  if (nums.size() >= 2) {
+    dp[0] = nums[0];
+    dp[1] = nums[0] + nums[1];
+    path[0] = 1;
+    path[1] = 1;
+    for (int i = 2; i < nums.size(); i++) {
+      int t1 = dp[i - 1] + nums[i];
+      int t2 = dp[i - 2] + nums[i];
+      int best = min(t1, t2);
+      dp[i] = best;
+      if (best == t1) {
+        path[i] += path[i - 1];
+      }
+      if (best == t2) {
+        path[i] += path[i - 2];
+      }
+    }
+    return {dp[nums.size() - 1], path[nums.size() - 1]};
+  } else {
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      sum += nums[i];
+    }
+    return {sum, 1};
+  }
+}
+
+int main() {
+
+  vector<char> nums = {'a', 'b', 'b', 'a', 'z', 'c', 'd', 'b'};
+  pair<int, int> res = path(nums);
+  cout << "The min cost required is: " << res.first << endl;
+  cout << "The number of ways are: " << res.second << endl;
+
+  return 0;
+}
