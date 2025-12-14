a1875c2ebfe3970c027827ae4b4e6fc6fa47eb4a
aanshik-dev <aanshiksinghtomar@gmail.com>
2025-12-14 23:30:02 +0530

    DSA - 14 Dec

---
+++ Algorithm/15Leetcode/162_peak.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+  int findPeakElement(vector<int> &nums) {
+    
+  }
+};
+
+int main() {
+  vector<int> nums = {1, 2, 1, 3, 5, 6, 4};
+  Solution sol;
+  int res = sol.findPeakElement(nums);
+  cout << res << endl;
+  return 0;
+}
