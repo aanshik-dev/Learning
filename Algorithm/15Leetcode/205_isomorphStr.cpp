9146065a24d2cf1bd78fe796eae5fd9c532229e1
aanshik-dev <aanshiksinghtomar@gmail.com>
2025-12-16 00:45:43 +0530

    DSA - 15 Dec

---
+++ Algorithm/15Leetcode/205_isomorphStr.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+      
+
+    }
+};
+
+
+int main() {
+  string s = "title", t = "paper";
+  Solution sol;
+  bool res = sol.isIsomorphic(s,t);
+  cout << res << endl;
+  return 0;
+}
