5d80cd22af29fcf388edd463398710d7f1135cfb
aanshik-dev <aanshiksinghtomar@gmail.com>
2025-12-28 00:20:55 +0530

    DSA - 27 Dec

---
+++ Algorithm/8LinkedList/876_middleLL.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+  vector<int> nums = {};
+  Solution sol;
+  int res = sol.singleNumber(nums);
+  cout << res << endl;
+  return 0;
+}
