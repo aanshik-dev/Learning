7d398d620f7c5051c387fd3e008c659f577dfce8
aanshik-dev <aanshiksinghtomar@gmail.com>
2025-12-27 23:12:42 +0530

    DSA - 27 Dec

---
+++ Algorithm/8LinkedList/237_delete.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+  public:
+  void deleteNode(ListNode *node) {
+    while(!node){
+      
+    }
+  }
+};
+
+int main() {
+  ListNode *root = new ListNode(2);
+  ListNode *n1 = new ListNode(5);
+  ListNode *n2 = new ListNode(-1);
+  ListNode *n3 = new ListNode(6);
+  ListNode *n4 = new ListNode(8);
+  root->next = n1;
+  n1->next = n2;
+  n2->next = n3;
+  n3->next = n4;
+
+  Solution sol;
+  sol.deleteNode(root);
+  cout << root->val << root->next->val << root->next->next->val << root->next->next->next->val << endl;
+  return 0;
+}
