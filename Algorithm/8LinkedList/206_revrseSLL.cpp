1995016f9e644ecea89cc648e0c6e29ecbed0ab9
aanshik-dev <aanshiksinghtomar@gmail.com>
2025-12-28 20:15:32 +0530

    DSA - 28 Dec

--- Algorithm/8LinkedList/206_revrseSLL.cpp
+++ Algorithm/8LinkedList/206_revrseSLL.cpp
@@ -4,7 +4,7 @@ using namespace std;
 struct ListNode {
   int val;
   ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+  ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
@@ -12,6 +12,27 @@ struct ListNode {
 class Solution {
   public:
   ListNode *reverseList(ListNode *head) {
+    ListNode *pre = nullptr;
+    ListNode *cur = head;
+    ListNode *nxt = nullptr;
+    while (cur) {
+      nxt = cur->next;
+      cur->next = pre;
+      pre = cur;
+      cur = nxt;
+    }
+    return pre;
+  }
+
+  // Recursive
+  ListNode *reverseList(ListNode *head) {
+    if (!head || !head->next) {
+      return head;
+    }
+    ListNode *res = reverseList(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+    return res;
   }
 };
 
@@ -22,10 +43,13 @@ int main() {
   ListNode *n3 = new ListNode(4, n4);
   ListNode *n2 = new ListNode(3, n3);
   ListNode *n1 = new ListNode(2, n2);
-  ListNode *root = new ListNode(1, n1);
+  ListNode *n0 = new ListNode(1, n1);
 
   Solution sol;
-  ListNode *res = sol.reverseList(root);
-  cout << res << endl;
+  ListNode *res = sol.reverseList(n0);
+  while (res != nullptr) {
+    cout << res->val << " ";
+    res = res->next;
+  }
   return 0;
 }
