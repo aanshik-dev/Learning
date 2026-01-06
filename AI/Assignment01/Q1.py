f39bbf257e958a187a74fca763625bda6cd239f9
aanshik-dev <aanshiksinghtomar@gmail.com>
2026-01-07 00:02:06 +0530

    Python

---
+++ AI/Assignment01/Q1.py
@@ -0,0 +1,19 @@
+x = 25
+print(x)
+print(type(x))
+
+y = 15.5
+add = x + y
+sub = x - y
+mul = x * y
+div = x / y
+print(add)
+print(sub)
+print(mul)
+print(div)
+
+str = "Aanshik"
+print(len(str))
+
+str = int(str)
+print(type(str))
