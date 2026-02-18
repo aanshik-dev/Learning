91c7ee77d59b21bc7624c37f63319675d84963a5
aanshik-dev <aanshiksinghtomar@gmail.com>
2026-02-18 19:33:00 +0530

    A star

--- AI/0Lab/Assignment06/A-star.py
+++ AI/0Lab/Assignment06/A-star.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import time
 import heapq
 import random
@@ -20,7 +22,7 @@ def getMoves(state, n):
   if row > 0: moves.append(swap(state, blank, blank-n))
   if row < n-1: moves.append(swap(state, blank, blank+n))
   if col > 0: moves.append(swap(state, blank, blank-1))
-  if row < n-1: moves.append(swap(state, blank, blank+1))
+  if col < n-1: moves.append(swap(state, blank, blank+1))
   return moves
 
 
@@ -46,52 +48,60 @@ def manHattan(state, n):
   return dis
 
 # h3 (Custom Heuristic): A custom heuristic of your design. It must be admissible.
+def customHur(state, n):
+  return max(misTiles(state, n), manHattan(state, n))
 
 
-def Astar(init, n):
+def Astar(init, n, heuristic):
   start = time.time()
   pq = []
-  heapq.heappush(pq, (misTiles(init), 0, init))
+  heapq.heappush(pq, (heuristic(init, n), 0, init))
   vis = {init: 0}
 
   while pq:
     if time.time() - start > timeout:
-      return "Timeout",timeout, len(vis)
-    f, cost, node = heapq.heappop(pq)
+      return "Timeout", timeout, len(vis)
+    f, g, node = heapq.heappop(pq)
 
     if node == goal: return "Success", time.time() - start, len(vis)
     for mov in getMoves(node, n):
-      newCost = cost + 1
+      newCost = g + 1
       if mov not in vis or newCost < vis[mov]:
         vis[mov] = newCost
-        newF =  newCost + misTiles(mov)
-        heapq.heappush(pq, (newF, newCost, mov))
+        heapq.heappush(pq,(newCost + heuristic(mov, n), newCost, mov))
   return "Failure", time.time() - start, len(vis)
 
 
-def greedBest(init, n):
+def greedyBFS(init, n, heuristic):
   start = time.time()
   pq = []
-  heapq.heappush(pq, (misTiles(init),0, init))
-  vis = {init: 0}
+  heapq.heappush(pq, (heuristic(init, n), init))
+  vis = set([init])
 
   while pq:
     if time.time() - start > timeout:
-      return "Timeout",timeout, len(vis)
-    hur, cost, node = heapq.heappop(pq)
+      return "Timeout", timeout, len(vis)
+    h, node = heapq.heappop(pq)
 
-    if node == goal: return "Success", start - time.time(), len(vis)
+    if node == goal: return "Success", time.time() - start, len(vis)
     for mov in getMoves(node, n):
-      newCost = cost + 1
-      if mov not in vis or newCost < vis[mov]:
-        vis[mov] = newCost
-        newF =  newCost + misTiles(mov)
-        heapq.heappush(pq, (misTiles(mov), newCost, mov))
-  return "Failure", start-time.time(), len(vis)
+      if mov not in vis:
+        vis.add(mov)
+        heapq.heappush(pq, (heuristic(mov, n), mov))
+  return "Failure", time.time() - start, len(vis)
 
 
 
+heuristics = {
+  "Misplaced": misTiles,
+  "Manhattan": manHattan,
+  "Custom": customHur
+}
 
+astar_time = {h: [] for h in heuristics}
+astar_nodes = {h: [] for h in heuristics}
+greedy_time = {h: [] for h in heuristics}
+greedy_nodes = {h: [] for h in heuristics}
 
 n = int(input(f"====== Menu ======\n8 - puzzle: 3 \n15 - puzzle: 4\nEnter the value of n: "))
 
@@ -99,7 +109,57 @@ goal = tuple(list(range(1,n*n)) + [0])
 
 for i in range(10):
   init = shuffle(goal,n)
-  print(init)
-  # AStar
-  print("Running A*...")
-  time, nodes = Astar()
+  print(f"\nInitial State {i+1}: {init}")
+
+  # AStar ---------------
+  print(" => Running A*...")
+  for hname, hfunc in heuristics.items():
+    status, sec, nodes = Astar(init, n, hfunc)
+    astar_time[hname].append(sec)
+    astar_nodes[hname].append(nodes)
+    print(f"  -> {hname} : {status}, Time: {sec:.2f}s, nodes Explored: {nodes}")
+  
+  # Greedy Best FS ---------------
+  print(" => Running Greedy BFS...")
+  for hname, hfunc in heuristics.items():
+    status, sec, nodes = greedyBFS(init, n, hfunc)
+    greedy_time[hname].append(sec)
+    greedy_nodes[hname].append(nodes)
+    print(f"  -> {hname} : {status}, Time: {sec:.2f}s, nodes Explored: {nodes}")
+
+print("")
+
+labels = list(heuristics.keys())
+
+astar_avg_time = np.array([np.mean(astar_time[h]) for h in heuristics])
+greedy_avg_time = np.array([np.mean(greedy_time[h]) for h in heuristics])
+
+astar_avg_nodes = np.array([np.mean(astar_nodes[h]) for h in heuristics])
+greedy_avg_nodes = np.array([np.mean(greedy_nodes[h]) for h in heuristics])
+
+
+x = range(len(labels))
+
+# Execution Time Plot
+plt.figure()
+plt.bar(x, astar_avg_time, width=0.4, label="A*", align='center')
+plt.bar([i+0.4 for i in x], greedy_avg_time, width=0.4, label="Greedy", align='center')
+plt.xticks([i+0.2 for i in x], labels)
+plt.ylabel("Average Time (seconds)")
+plt.title(f"Average Execution Time (n={n})")
+plt.legend()
+plt.show()
+
+# Nodes Expanded Plot
+plt.figure()
+plt.bar(x, astar_avg_nodes, width=0.4, label="A*", align='center')
+plt.bar([i+0.4 for i in x], greedy_avg_nodes, width=0.4, label="Greedy", align='center')
+plt.xticks([i+0.2 for i in x], labels)
+plt.ylabel("Average Nodes Expanded")
+plt.title(f"Nodes Expanded (n={n})")
+plt.legend()
+plt.show()
+
+
+
+
--- DBMS/0Lab/Assignment05/Lab5.sql
+++ DBMS/0Lab/Assignment05/Lab5.sql
@@ -1,8 +1,20 @@
 use lab;
+use company;
 select * from student;
 select * from faculty; 
 select * from enrolled;
 select * from class;
+
+create database company;
+create table employees (
+    person varchar(20),
+    supervisor varchar(20)
+);
+insert into employees values
+('Ravi', 'Aman'),
+('Mary', 'Sujata'),
+('Aman', 'Devi'),
+('Devi', 'Mary');
 
 -- Q1 Find the names of all classes that either meet in room 20 AVW or have five or more students enrolled.
 select name, room from class 
@@ -12,9 +24,68 @@ where room = "20 AVW" or name in (select cname from enrolled group by cname havi
 select room, count(*) as class from class group by room;
 
 -- Q3 List all faculty members, showing their id, name and the number of classes they teach.
--- The number of classes of those who teach no classes must be shown as 0.
+-- The number of classes of those who teach no classes must be shown as 0. 
 select f.fid, f.fname, count(c.name) from faculty f left outer join class c on f.fid = c.fid group by fid, fname; 
 
 -- Q4 List all the courses with their names, where they are taught and the number of students enrolled for each. 
 -- If no students are enrolled, show the number of students as 0.
-select c.name from class c left outer join enrolled e on c.name = e.cname group by e.cname;
+select c.name, c.room, count(e.snum) as students
+from class c left join enrolled e on c.name = e.cname group by c.name, c.room;
+
+-- Q5 Find all faculty members who belong to department 20 and whose courses are conducted in room R128
+select distinct f.fname from faculty f join class c on f.fid = c.fid
+where f.deptid = 20 and c.room = 'R128';
+
+-- Q6 Find the maximum age of all students of each major
+select major, max(age) as max_age from student group by major;
+
+-- Q7 Find the names of students and faculty members whose names contain the string “son”.
+select sname as name from student
+where sname like '%son%'
+union 
+select fname as name from faculty
+where fname like '%son%';
+
+-- Q8 Find all faculty members who belong to department 20 and whose courses are conducted in room R128. Use a nested query.
+-- Do not create views or use a“with” statement.
+select fname from faculty where deptid = 20 and fid in (
+select fid from class where room = 'R128' );
+
+-- Q9 Find all faculty members who belong to department 20 and whose courses are conducted in room R128. 
+-- Answer the question by creating a view and using it, without using a nested query.
+create view r128_faculty as
+select distinct fid
+from class
+where room = 'R128';
+
+select f.fname
+from faculty f
+join r128_faculty r
+on f.fid = r.fid
+where f.deptid = 20;
+
+-- Q10 Find the supervisor of Ravi. 
+select supervisor from employees where person = 'Ravi';
+
+-- Q11 Find the supervisor of the supervisor of Ravi. 
+select e2.supervisor from employees e1 join employees e2 on e1.supervisor = e2.person
+where e1.person = 'Ravi';
+
+-- Q12 Find all the supervisors (direct and indirect) of Ravi.
+with recursive supervisors as (
+    select supervisor
+    from employees
+    where person = "Ravi"
+    union
+    select e.supervisor
+    from employees e
+    join supervisors s
+    on e.person = s.supervisor
+)
+select supervisor
+from supervisors;
+
+
+
+
+
